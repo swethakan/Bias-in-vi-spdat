4c075c53f33508b315c2606ed150e8b8c3e4de79
Swetha <kan_swetha@yahoo.com>
2022-05-04 13:56:12 -0400

    find data now maybe

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -516,16 +516,3 @@ d3.csv("./assets/data/race.csv", function(error, data){
 // 	});
 // });
   
-var lastScrollTop = 0;
-var scrolling = "down"
-
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-   if (st > lastScrollTop){
-    scrolling = "down";
-   } else {
-    scrolling = "up"
-   }
-   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
